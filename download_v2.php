e4a198c0ec1772421661cd389ca1422a7ab673cf
Nishant6000 <67912173+Nishant6000@users.noreply.github.com>
2021-01-22 01:14:14 +0530

    correction3

--- download_v2.php
+++ download_v2.php
@@ -4,7 +4,6 @@
 if(!empty($_GET['f'])){
     $fileName = $_GET['f'];
 	$fileName2 = urldecode(str_ireplace('./optin_db_folder_v2','',$fileName));
-	echo $fileName2;die;
     $filePath = 'optin_db_folder_v2/'.$fileName2;
     if(!empty($fileName) && file_exists($filePath)){
 		echo "Your Download will start Now...";
